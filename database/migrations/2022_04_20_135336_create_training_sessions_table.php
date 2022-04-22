2f2d6964f03924eddf8f21da79124f0f8d25cbc1
Cyprien <rimbau15cy@lycee-ndduroc.com>
2022-04-22 11:44:53 +0200

    Add training session

---
+++ database/migrations/2022_04_20_135336_create_training_sessions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainingSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('training_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->date('dateSession');
+            $table->unsignedBigInteger('program_id');
+            $table->foreign('program_id')->references('id')->on('programs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('training_sessions');
+    }
+}
